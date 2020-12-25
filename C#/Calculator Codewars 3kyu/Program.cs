195f1d3c83ef91798301e75e03248a1451f3842c
AfroKakTyC <63326960+AfroKakTyC@users.noreply.github.com>
2020-12-25 16:38:50 +0300

    2 katas added

---
+++ C#/Calculator Codewars 3kyu/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator_Codewars_3kyu
+{
+	class Program
+	{
+		public static bool IsNumber(string str)
+		{
+			char[] numChars = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.' };
+			foreach (char ch in str)
+				if (Array.IndexOf(numChars, ch) < 0)
+					return (false);
+			if (str.Length > 0)
+				return (true);
+			else
+				return (false);
+		}
+
+		public static int CheckPriority(string str)
+		{
+			if (str == "+" || str == "-")
+				return (1);
+			else if (str == "*" || str == "/")
+				return (2);
+			return (0);
+		}
+
+		public static double DoOperation(double firstNum, double secondNum, string operation)
+		{
+			if (operation == "+")
+				return (secondNum + firstNum);
+			else if (operation == "-")
+				return (secondNum - firstNum);
+			else if (operation == "*")
+				return (secondNum * firstNum);
+			else
+				return (secondNum / firstNum);
+		}
+
+		public static double Evaluate(string expression)
+		{
+			Stack<double> nums = new Stack<double>();
+			Stack<string> operators = new Stack<string>();
+			string[] numsAndOperators = expression.Split(' ');
+			for (int i = 0; i < numsAndOperators.Length; i++)
+			{
+				if (IsNumber(numsAndOperators[i]))
+					nums.Push(Double.Parse(numsAndOperators[i]));
+				else
+				{
+					if (numsAndOperators[i] == "(")
+						operators.Push(numsAndOperators[i]);
+					else if (numsAndOperators[i] == ")")
+					{
+						while (operators.Peek() != "(")
+						{
+							if (nums.Count == 1)
+							{
+								if (operators.Peek() == "-")
+									nums.Push(-nums.Pop());
+								operators.Pop();
+							}
+							else
+							{
+								double firstNum = nums.Pop();
+								double secondNum = nums.Pop();
+								string operation = operators.Pop();
+								nums.Push(DoOperation(firstNum, secondNum, operation));
+							}
+						}
+						operators.Pop();
+					}
+					else
+					{
+						if (operators.Count == 0 || CheckPriority(numsAndOperators[i]) > CheckPriority(operators.Peek()))
+							operators.Push(numsAndOperators[i]);
+						else
+						{
+							while (operators.Count > 0 && CheckPriority(numsAndOperators[i]) <= CheckPriority(operators.Peek()))
+							{
+								Console.WriteLine(operators.Count);
+								if (nums.Count == 1)
+								{
+									if (operators.Peek() == "-")
+										nums.Push(-nums.Pop());
+									operators.Pop();
+								}
+								else
+								{
+									double firstNum = nums.Pop();
+									double secondNum = nums.Pop();
+									string operation = operators.Pop();
+									nums.Push(DoOperation(firstNum, secondNum, operation));
+								}
+							}
+							operators.Push(numsAndOperators[i]);
+						}
+					}
+				}
+			}
+			while (operators.Count > 0)
+			{
+				if (nums.Count == 1)
+				{
+					if (operators.Peek() == "-")
+						nums.Push(-nums.Pop());
+					operators.Pop();
+				}
+				else
+				{
+					double firstNum = nums.Pop();
+					double secondNum = nums.Pop();
+					string operation = operators.Pop();
+					nums.Push(DoOperation(firstNum, secondNum, operation));
+				}
+			}
+			return (nums.Pop());
+		}
+
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Result = {0}", Evaluate("1 + 2 * ( 3 + 4 / 2 - ( 1 + 2 ) ) * 2 + 1"));
+		}
+	}
+}
---
+++ C#/Human readable duration format Codewars 4kyu/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Human_readable_duration_format_Codewars_4kyu
+{
+	class TimeUnit
+	{
+		public int Value { get; private set; }
+		
+		public string Name { get; private set; }
+
+		public TimeUnit(int value, string name)
+		{
+			Value = value;
+			Name = name;
+		}
+			
+	}
+
+	class Program
+	{
+		public static string formatDuration(int seconds)
+		{
+			List<TimeUnit> timeUnits = new List<TimeUnit>();
+
+			int years = seconds / 60 / 60 / 24 / 365;
+			if (years > 0)
+				timeUnits.Add(new TimeUnit(years, "year"));
+			seconds = seconds - years * 60 * 60 * 24 * 365;
+			int days = seconds / 60 / 60 / 24;
+			if (days > 0)
+				timeUnits.Add(new TimeUnit(days, "day"));
+			seconds = seconds - days * 60 * 60 * 24;
+			int hours = seconds / 60 / 60;
+			if (hours > 0)
+				timeUnits.Add(new TimeUnit(hours, "hour"));
+			seconds = seconds - hours * 60 * 60;
+			int minutes = seconds / 60;
+			if (minutes > 0)
+				timeUnits.Add(new TimeUnit(minutes, "minute"));
+			seconds = seconds - minutes * 60;
+			if (seconds > 0)
+				timeUnits.Add(new TimeUnit(seconds, "second"));
+
+			string result = "";
+			for (int i = 0; i < timeUnits.Count; i++)
+			{
+				result += timeUnits[i].Value + " " + timeUnits[i].Name;
+				if (timeUnits[i].Value > 1)
+					result += "s";
+				if (timeUnits.Count - i == 2)
+					result += " and ";
+				else if (timeUnits.Count - i > 1)
+					result += ", ";
+			}
+			if (timeUnits.Count == 0)
+				result += "now";
+			return (result);
+		}
+
+		static void Main(string[] args)
+		{
+			//formatDuration(121);
+			Console.WriteLine(formatDuration(242062374));
+		}
+	}
+}
