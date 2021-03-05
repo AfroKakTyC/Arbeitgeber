eea54190970210f04ecc8386341ab46c67875cd6
AfroKakTyC <63326960+AfroKakTyC@users.noreply.github.com>
2021-03-05 20:24:28 +0300

    encrypt-decrypt added

--- Java/encryptdecrypt/Main.java
+++ Java/encryptdecrypt/Main.java
@@ -1,3 +1,13 @@
+//Extend your program by adding different algorithms to encode/decode data. The first one would be shifting algorithm
+//		(it shifts each letter by the specified number according to its order in the alphabet in circle).
+//		The second one would be based on Unicode table, like in the previous stage.
+//
+//		When starting the program, the necessary algorithm should be specified by an argument (-alg).
+//		The first algorithm should be named shift, the second one should be named unicode. If there is no -alg you should default it to shift.
+//
+//		Remember that in case of shift algorithm you need to encode only English letters
+//		(from 'a' to 'z' as the first circle and from 'A' to 'Z' as the second circle i.e. after 'z' comes 'a' and after 'Z' comes 'A')
+
 package encryptdecrypt;
 
 import java.io.FileNotFoundException;
